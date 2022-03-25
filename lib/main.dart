93b61f6a438da71dc945caff11a5ca71ce6d3dbf
qkrtnfks128 <sulan6789@naver.com>
2022-03-25 12:44:37 +0900

    v.0.1 - 박수란작업

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:fietmobile/routes.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,16 +9,31 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      initialRoute: '/',
-      routes: routes,
+    return ScreenUtilInit(
+        designSize: const Size(390, 844),
+        minTextAdapt: true,//너비와 높이의 최소값에 따라 텍스트를 조정할지 여부
+        splitScreenMode: true,//	분할 화면 지원
+        builder: () =>
+         MaterialApp(
+          title: 'Flutter Demo',
+           builder: (context, widget) {
+             ScreenUtil.setContext(context);
+             return MediaQuery(
+               data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
+               child: widget!,
+             );
+           },
+          theme: ThemeData(
+              primarySwatch: Colors.blue,
+              appBarTheme: const AppBarTheme(backgroundColor:Colors.white ,elevation: 0, titleTextStyle:TextStyle(color:Colors.black,fontSize:14) ),//공통앱바
+              scaffoldBackgroundColor:  Colors.white//공통배경색
+          ),
+          initialRoute: '/',
+          routes: routes,
+        )
+
     );
   }
 }
--- lib/routes.dart
+++ lib/routes.dart
@@ -1,6 +1,8 @@
 import 'package:fietmobile/ui/views/navigate_page.dart';
+import'ui/views/evaluation/pages/evaluation_page.dart';
 import 'package:flutter/material.dart';
 
 var routes = <String, WidgetBuilder>{
   '/': (_) => const NavigatePage(),
+  '/evaluation':(_) =>const EvaluationPage()
 };
