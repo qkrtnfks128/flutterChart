54730a1ef15eada09dc33998d8ccddc8d5c00b3e
David Choi <davidchoi0531@gmail.com>
2022-03-28 14:02:10 +0900

    no message

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,5 +1,5 @@
 import 'package:fietmobile/ui/views/navigate_page.dart';
-import'ui/views/evaluation/pages/evaluation_page.dart';
+import 'ui/views/evaluation/pages/evaluation_page.dart';
 import 'package:flutter/material.dart';
 
 var routes = <String, WidgetBuilder>{
